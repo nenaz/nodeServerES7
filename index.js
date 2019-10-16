41aee10243e49f47bd932df78e6269ba4fa8995a
mrnenaz <mrnenaz@gmail.com>
2019-10-16 13:18:28 +0300

    start config

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const path = require('path')
+import path from 'path';
+const PORT = 5001
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
